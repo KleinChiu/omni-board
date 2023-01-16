5fb683ce72a7d2861e3cdc933c49c126527c7c23
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2023-01-16 08:40:08 +0000

    feat(board): :sparkles: Add boards page

--- plugins/routers.ts
+++ plugins/routers.ts
@@ -1,9 +1,15 @@
 /// <reference path="../env.d.ts" />
 
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
 export default createRouter({
-  routes: [{ path: "/", component: () => import("../views/Index.vue") }],
+  routes: [
+    { path: "/", component: () => import("../views/Index.vue") },
+    {
+      path: "/boards",
+      component: () => import("../views/Boards.vue"),
+    },
+  ],
   history: createWebHistory(process.env.npm_package_name),
   strict: true,
 });
---
+++ views/Boards.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2 @click="changeBoardTitle">
+    {{ boardTitle.toUpperCase() }}
+  </h2>
+
+  <router-view />
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const boardTitle = ref("score board");
+const changeBoardTitle = () =>
+  Promise.resolve().then(() => {
+    const title = prompt("Board title?");
+    if (title == null) {
+      return;
+    }
+    boardTitle.value = title;
+  });
+</script>
